c9405a93f5229fecc30b7edf060b4eb4161791ca
Bach Hoang <148547328+TBach17112003@users.noreply.github.com>
2024-07-11 16:20:32 +0700

    Add files via upload

--- client_Phase1_GoLeft.py
+++ client_Phase1_GoLeft.py
@@ -13,7 +13,6 @@ from utime import sleep_ms, ticks_ms  # Import delay function and ticks for timi
 
 
 # Setup Ports, Motors, Sensors
-vcp = USB_VCP(0)
 MotorA = port.A.motor        # MotorA defines the port A of Hub
 MotorB = port.B.motor        # MotorB defines the port B of Hub
 MotorB.default(max_power = 50, stop = 2)
@@ -28,14 +27,13 @@ message = bytes([])
 def get_State():
     global safe_State, motor_running
     # If either motor is running, set safe_State to False
-    if MotorA.busy(1) or MotorB.busy(1):
-        safe_State = False
-        motor_running = True
-    else:
+    if motor_running == False:
         safe_State = True
-        motor_running = False
+        # motor_running = True
+    else:
+        safe_State = False
 
-def response_Confirmation():
+def response_Confirmation(vcp):
     hub.led(2)
     sleep_ms(500)
     message = bytes([35, 1, 121, 0, 0, 0, 0, 0, 0])
@@ -43,15 +41,16 @@ def response_Confirmation():
     sleep_ms(100)
     if safe_State == True:
         request_flash_SW()
+        
 
-def response_Flash_Status():
+def response_Flash_Status(vcp):
     hub.led(1)
     sleep_ms(500)
     message = bytes([35, 1, 124, 0, 0, 0, 0, 0, 0])
     vcp.write(message)
     sleep_ms(100)
 
-def request_flash_SW():
+def request_flash_SW(vcp):
     hub.led(3)
     sleep_ms(500)
     message = bytes([35, 1, 122, 0, 0, 0, 111, 0, 0])
@@ -64,7 +63,8 @@ def classify_Command(command):
     elif command[1] == 123:
         response_Flash_Status()
 
-def handle_VCP():
+def handle_VCP(vcp):
+    
     if vcp.isconnected():  
         # vcp.CTS
         if vcp.any():
@@ -75,17 +75,28 @@ def handle_VCP():
             sleep_ms(100)
 
 def run_motor():
-    global motor_running, motor_end_time
-    if not motor_running:
-        MotorA.run_for_degrees(30)
-        MotorB.run_for_time(3000, speed = 50)
-        motor_running = True
-        motor_end_time = ticks_ms() + 3000  # Set the motor end time
-        sleep_ms(500)
+    global motor_running, motor_end_time, motor_start_time
+    MotorA.run_for_degrees(30, speed = 50)
+    MotorB.run_at_speed(-50)  # Run the motor at a constant speed
+    motor_start_time = ticks_ms()  # Record the start time of motor running
+    motor_running = True
+    motor_end_time = motor_start_time + 5000  # Set the motor end time to 5 seconds after start
 
-while True:
-    if safe_State == False:
-        run_motor()
-    get_State()
-    handle_VCP()
-    
+def main():
+    vcp = USB_VCP(0)
+    vcp.setinterrupt(3)
+    current_time = ticks_ms()
+    while True:
+        if not motor_running:
+            run_motor()
+            safe_State = False 
+        elif motor_running:
+            # Check if motor has been running for more than 5 seconds
+            if current_time >= motor_end_time:
+                MotorB.brake()  # Brake the motor
+                motor_running = False
+                # Turn to safe state and wait for 2 seconds
+                safe_State = True
+                sleep_ms(2000)
+    sleep_ms(50)
+main()
